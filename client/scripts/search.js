382f2548382dc93b78fb2f63c2bd2c3560ec7379
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-02-18 23:35:57 -0800

    Update endpoints

--- client/scripts/search.js
+++ client/scripts/search.js
@@ -1,4 +1,4 @@
-const endPointRoot = "https://comp4537-lab04-server.vercel.app/";
+const endPointRoot = "https://comp4537-lab04-server.vercel.app/api/server.js/";
 // const endPointRoot = "http://localhost:8000/";
 class Search {
     getResults(query) {
--- client/scripts/store.js
+++ client/scripts/store.js
@@ -1,5 +1,5 @@
 import message from "../user.js";
-const endPointRoot = "https://comp4537-lab04-server.vercel.app/";
+const endPointRoot = "https://comp4537-lab04-server.vercel.app/api/server.js/";
 // const endPointRoot = "http://localhost:8000/";
 class Store {
     createWord(word, definition) {
